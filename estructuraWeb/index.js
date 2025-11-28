1d631145fdb0901c84132f0eb11c98cc405b4fbb
= <=>
2025-11-28 13:55:09 +0100

    clase 28/11/2025

---
+++ estructuraWeb/index.js
@@ -0,0 +1,67 @@
+let section2= document.getElementById("section2");
+
+let titulo = "mi titulo 2"
+
+let texto= "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi saepe, unde nesciunt expedita qui sit dolorem nam inventore repellat maxime atque quaerat nihil, molestiae autem ratione odio eligendi laboriosam corporis?";
+
+let img = "img/main_image_star-forming_region_carina_nircam_final-5mb.webp";
+
+let articulo = document.createElement("article");
+
+let section3 = document.getElementById("section3");
+let contenedor = document.getElementById("contenedor");
+
+
+articulo.innerHTML= `
+<div class="titulo">
+    <h1>  ${titulo}  </h1>
+</div>
+
+<div class="texto">
+    ${texto}
+</div>
+
+<div class="imagen">
+    <img src="${img}">
+</div>
+`;
+
+articulo.className="articulo2";
+section2.appendChild(articulo);
+
+
+//consumir el json
+
+fetch("/productos.json")
+.then(response => response.json())
+.then(data => {
+
+    data.productos.forEach(p => {
+        
+        contenedor.innerHTML+=`
+        
+            <div class="card">
+                <div class="titulo">
+                    <h2>${p.nombre}</h2>
+                </div>
+                <div class="imagen">
+                    <img class="img" src="${p.imagen}">
+                </div>
+                <div class="precio">
+                    <p>Precio: ${p.precio} €</p>
+                </div>
+
+                <div class="botones"> 
+                    <button>Comprar</button>
+                 </div>
+            </div>
+        `;
+
+        section3.appendChild(contenedor)
+        
+    });//fin del for
+
+
+})//fin del then
+.catch(e => console.log(e))
+
